61e5786f0bb9ac116e7538e8299f5c62766ffb44
muabnezor <adam.rosenbaum@regionvasterbotten.se>
2026-04-30 10:30:14 +0200

    fix action tests

--- tests/test_action_generate_load_config.py
+++ tests/test_action_generate_load_config.py
@@ -38,18 +38,28 @@ class GenerateLoadConfigActionTestCase(BaseActionTestCase):
                 touch(p)
             sample_files = {sample_id: [{"path": str(p)} for p in sample_paths]}
 
+            sample_info = {
+                "SAMPLE1": {"Sex": "F"}
+            }
+
             action = self.get_action_instance(
                 config={
                     "owners_map": {PIPELINE_RD: "clingen-rd"},
                     "genomes_map": {PIPELINE_RD: "38"},
+                    "rankmodel_map": {PIPELINE_RD: "rankmodel_v1.ii"},
+                    "scout_case_file_suffixes": {"vcf_snv": "_snv_ranked_clinical.vcf.gz"},
+                    "scout_sample_file_suffixes": {"alignment_path": "_sorted_md.bam", "d4_file": ".d4"},
+                    "scout_chromograph_file_prefixes": {"autozygous": "_rhocallviz_chr"}
                 }
             )
+
 
             success, result = action.run(
                 sample_ids=[sample_id],
                 case_id=case_id,
                 case_name="Test family",
                 sample_files=sample_files,
+                sample_info=sample_info,
                 case_files=case_files,
                 pipeline=PIPELINE_RD,
                 igene_panels=["CARDIO_PAN_WGS_v1.0"],
@@ -63,12 +73,14 @@ class GenerateLoadConfigActionTestCase(BaseActionTestCase):
             self.assertEqual(result["human_genome_build"], "38")
             self.assertIn("analysis_date", result)
             self.assertIn("gene_panels", result)
-            self.assertIn("default_panels", result)
+            self.assertIn("default_gene_panels", result)
 
             # --- sample-level assertions ---
             self.assertEqual(len(result["samples"]), 1)
+            self.assertEqual(result["samples"][0]["sex"], "female")
             sample = result["samples"][0]
 
             self.assertIn("alignment_path", sample)
-            self.assertIn("d4_path", sample)
+            self.assertIn("d4_file", sample)
             self.assertIn("chromograph_images", sample)
+
