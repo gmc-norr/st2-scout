0b2c083c56f62e187c074a543552f9f788e74a68
muabnezor <adam.rosenbaum@regionvasterbotten.se>
2026-08-26 12:17:20 +0200

    Handle custom gms560 panels

--- actions/generate_load_config.py
+++ actions/generate_load_config.py
@@ -47,7 +47,7 @@ class GenerateLoadConfigAction(Action):
     def _get_scout_panels(self, panels: list, global_panels: tuple) -> tuple:
         default_panels = []
         for p in panels:
-            if p == "SNV_WGS":
+            if p == "SNV_WGS" or p == "CustomSolid_PAN_GMS560":
                 continue
             default_panels.append(self._scout_panel_from_igene_panel(p))
             log.info(f"using scout panel {default_panels[-1]} (iGene panel {p})")
@@ -271,3 +271,4 @@ class GenerateLoadConfigAction(Action):
 
         data = dict(zip(header, values))
         return str(float(data["%"]))
+
